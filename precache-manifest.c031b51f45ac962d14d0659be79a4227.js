213b6b9b7cc9f5bff704c0dcc1c801201eb0fa65
Abdelrahman Dawood <daoud728@gmail.com>
2020-10-23 13:32:36 +0200

    Updates

---
+++ precache-manifest.c031b51f45ac962d14d0659be79a4227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225a0de7b31cf2304a65d8deba20ad51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5936bf764049fd2bde14",
+    "url": "/static/css/main.61fd2d3b.chunk.css"
+  },
+  {
+    "revision": "f00e66eb01adfbffda5b",
+    "url": "/static/js/2.2a9ad69d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2a9ad69d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5936bf764049fd2bde14",
+    "url": "/static/js/main.75da36d5.chunk.js"
+  },
+  {
+    "revision": "c7c122fa68a5649f0838",
+    "url": "/static/js/runtime-main.bebe8c53.js"
+  }
+]);
